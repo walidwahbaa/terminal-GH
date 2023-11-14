09f4e38c26f74933cc0844a4ead2c3a2a6cc8c8a
walidwahbaa <walidwahba5000@gmail.com>
2023-11-14 21:49:11 +0300

    inial commit

---
+++ UsingGitTerminal/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  UsingGitTerminal
+//
+//  Created by walid wahba on 14.11.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ UsingGitTerminal/UsingGitTerminalApp.swift
@@ -0,0 +1,17 @@
+//
+//  UsingGitTerminalApp.swift
+//  UsingGitTerminal
+//
+//  Created by walid wahba on 14.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct UsingGitTerminalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
