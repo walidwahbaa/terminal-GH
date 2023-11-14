8f3b58ed65272e3595762b6cedd4992b7539ad1e
walidwahbaa <walidwahba5000@gmail.com>
2023-11-14 22:11:34 +0300

    Add image

--- UsingGitTerminal/ContentView.swift
+++ UsingGitTerminal/ContentView.swift
@@ -10,9 +10,9 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "figure.wave")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
+            Image(systemName: "figure.wave").resizable()
+                .foregroundColor(.green)
+                .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: 150, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
             Text("Hello, world!")
         }
         .padding()
